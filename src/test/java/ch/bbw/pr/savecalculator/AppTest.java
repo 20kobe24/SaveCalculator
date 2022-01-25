9c9fc946f6c72c51bfaaa43ad35108ce440081b8
Ryan <ryan.cybergen@gmail.com>
2022-01-26 00:07:55 +0100

    feat: Summe und Subtraktion

--- src/test/java/ch/bbw/pr/savecalculator/AppTest.java
+++ src/test/java/ch/bbw/pr/savecalculator/AppTest.java
@@ -2,6 +2,8 @@ package ch.bbw.pr.savecalculator;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,6 +14,23 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    }
+
+    @Test
+    public void testSummeZweiNegativeIsOk() {
+        int value1 = -150;
+        int value2 = -200;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.summe(value1, value2) == -350);
+    }
+
+    @Test
+    public void testSummeZweiPositiveIsOk() {
+        int value1 = 150;
+        int value2 = 150;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.summe(value1, value2) == 300);
+    }
     @Test
     public void shouldAnswerWithTrue()
     {
