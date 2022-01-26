428bdbdccb8ebe074a44fccc308dbcbf43088d10
Ryan <ryan.cybergen@gmail.com>
2022-01-26 01:01:03 +0100

    feat: Äquivalenzklassen

--- src/main/java/ch/bbw/pr/savecalculator/SaveCalculator.java
+++ src/main/java/ch/bbw/pr/savecalculator/SaveCalculator.java
@@ -18,16 +18,69 @@ public class SaveCalculator {
 	}
 
 	// Noch un-safe Methoden, die müssen noch angepasst werden.
+	public int subtract(int subtract1, int subtract2) throws ArithmeticException {
+		long value = (long) subtract1 - (long) subtract2;
+		if ((value > Integer.MAX_VALUE) || (value < Integer.MIN_VALUE)) {
+			throw new ArithmeticException();
+
+		}
+		return subtract1 - subtract2;
+	}
+
 	public int subtraktion(int value1, int value2) {
+		long value = (long) value1 - (long) value2;
+		if ((value > Integer.MAX_VALUE) || (value < Integer.MIN_VALUE)) {
+			throw new ArithmeticException();
+		}
 		return value1 - value2;
 	}
 
-	public double division(int value1, int value2) {
+	public double divide(int value1, int value2) {
+		if (value2 == 0)
+			throw new ArithmeticException("division on zero");
+		long value = (long) value1 / (long) value2;
+		if ((value > Integer.MAX_VALUE) || (value < Integer.MIN_VALUE)) {
+			throw new ArithmeticException();
+		}
 		return value1 / value2;
 	}
 
-	private double multiplication(Integer value1, Integer value2) {
+	public double multiply(Integer value1, Integer value2) {
+		long value = (long) value1 * (long) value2;
+		if ((value > Integer.MAX_VALUE) || (value < Integer.MIN_VALUE)) {
+			throw new ArithmeticException();
+		}
 		return value1 * value2;
 	}
 
+	// math.pow is applied 
+	protected int pow(int value, int powValue) {
+		return (int) Math.pow(value, powValue);
+	}
+
+	// calculating SQRT
+	public int sqrt(int value) {
+		if (value == 0)
+			return 0;
+		if (value < 0)
+			throw new ArithmeticException();
+		return (int) Math.sqrt(value);
+	}
+
+	// calculate rest with modulo
+	public int modulo(int a, int b) {
+		return a % b;
+	}
+
+	public String decToBinary(int decNumber) {
+		return Integer.toBinaryString(decNumber);
+	}
+
+	public String decToHex(int decNumber) {
+		return Integer.toHexString(decNumber).toUpperCase();
+	}
+
+	public String decToOctal(int decNumber) {
+		return Integer.toOctalString(decNumber).toUpperCase();
+	}
 }
--- src/test/java/ch/bbw/pr/savecalculator/AppTest.java
+++ src/test/java/ch/bbw/pr/savecalculator/AppTest.java
@@ -1,10 +1,12 @@
 package ch.bbw.pr.savecalculator;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Unit test for simple App.
@@ -53,6 +55,264 @@ public class AppTest
     
     protected int pow(int value, int powValue) {
         return (int) Math.pow(value, powValue);
+    }
+
+    
+    @Test
+    public void testsummezweiPositiveMethodenIstOK() {
+        int value1 = 25;
+        int value2 = 25;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.summe(value1, value2) == 50);
+    }
+
+    @Test
+    public void testsummezweiNegativeMethodenIstOK() {
+        int value1 = -46;
+        int value2 = -20;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.summe(value1, value2) == -66);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void TestSummeGroesstePositiveundKleinstePositiveMethodenIstOk() {
+        int value1 = Integer.MAX_VALUE;
+        int value2 = 1;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.summe(value1, value2) == -2147483648);
+    }
+
+    @Test
+    public void testsummezweinegativezahlenIsOk() {
+        int value1 = -150;
+        int value2 = -200;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.summe(value1, value2) == -250);
+    }
+
+    @Test
+    public void testsummeZweiPositiveZahlenIsOk() {
+        int value1 = 420;
+        int value2 = 69;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.summe(value1, value2) == 489);
+    }
+
+    @Test
+    public void SummetestpositivenegativezahlenistOK() {
+        int value1 = 5;
+        int value2 = -10;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.summe(value1, value2) == -5);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void SummetestgrosstepositiveundKleinstepositiveZahlThrowsexpectedExpetionOK() {
+        int value1 = Integer.MAX_VALUE;
+        int value2 = 1;
+        SaveCalculator testee = new SaveCalculator();
+        testee.summe(value1, value2);
+    }
+
+    @Test
+    public void SummegreosstnegativeundkleinstpositiveIstOK() {
+        int value1 = Integer.MIN_VALUE;
+        int value2 = 1;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.summe(value1, value2) == -2147483648);
+    }
+
+    @Test
+    public void testSubtraktionVonzweiositiveZahlenIstOk() {
+        int value1 = 20;
+        int value2 = 40;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.subtract(value1, value2) == -20);
+    }
+
+    @Test
+    public void testSubtraktionVonzweinegativeZahlenIstOk() {
+        int value1 = -80;
+        int value2 = -110;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.subtract(value1, value2) == 30);
+    }
+
+    @Test
+    public void SubtraktionvonNegativeundPositiveisOk() {
+        int value1 = 2024;
+        int value2 = -2026;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.subtract(value1, value2) == 4050);
+    }
+
+    @Test
+    public void SubtraktiongroesstenegativeundkleinstenegativeIsOk() {
+        int value1 = Integer.MIN_VALUE;
+        int value2 = -1;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.subtract(value1, value2) == -2147483647);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void SubtraktiongroesstePositiveundKleinsteNegativezahlThrowsExpectedExpectionIstOK() {
+        int value1 = Integer.MAX_VALUE;
+        int value2 = -1;
+        SaveCalculator testee = new SaveCalculator();
+        testee.subtract(value1, value2);
+    }
+
+    @Test
+    public void SubtraktionPositiveUndNullMethodenIsOk() {
+        int value1 = 8;
+        int value2 = 0;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.subtract(value1, value2) == 8);
+    }
+
+    @Test
+    public void SubtraktionNegataiveUndNullMethodenIsOk() {
+        int value1 = -3;
+        int value2 = 0;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.subtract(value1, value2) == -3);
+    }
+
+    @Test
+    public void SubtratktiongroesstepositiveUndkleinstePositiveZahlIsOk() {
+        int value1 = Integer.MAX_VALUE;
+        int value2 = 1;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.subtract(value1, value2) == 2147483646);
+        testee.subtract(value1, value2);
+    }
+
+    @Test
+    public void DivisionzweiPositiveZahlenIsOK() {
+        int value1 = 35;
+        int value2 = 7;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.divide(value1, value2) == 5);
+    }
+
+    @Test
+    public void DivisionzweiNegativeZahlenIsOK() {
+        int value1 = -100;
+        int value2 = -25;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.divide(value1, value2) == 4);
+    }
+
+    @Test
+    public void DivisionPositivundnegativeZahlIsOk() {
+        int value1 = 200;
+        int value2 = -4;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.divide(value1, value2) == -25);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void DivisionpositivezahlUndnullthrowsexpectedexceptionistok() {
+        int value1 = 19;
+        int value2 = 0;
+        SaveCalculator testee = new SaveCalculator();
+        testee.divide(value1, value2);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void Divisionnegativezahlnndnullthrowsexpectedexceptionistok() {
+        int value1 = -9;
+        int value2 = 0;
+        SaveCalculator testee = new SaveCalculator();
+        testee.divide(value1, value2);
+    }
+
+    @Test
+    public void Divisiongrosstositivezahlkleinstepositivethrowsexpectedexceptionisok() {
+        int value1 = Integer.MAX_VALUE;
+        int value2 = 1;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.divide(value1, value2) == 2147483647);
+    }
+
+    @Test
+    public void Divisiongrosstenegativekleinstepositiveisok() {
+        int value1 = Integer.MIN_VALUE;
+        int value2 = 1;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.divide(value1, value2) == -2147483648);
+    }
+
+    @Test
+    public void Divisiongrosstepositivezahlkleinstenegativeisok() {
+        int value1 = Integer.MAX_VALUE;
+        int value2 = -1;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.divide(value1, value2) == -2147483647);
+    }
+
+    @Test
+    public void Multiplikationvonpositivezahlenmitprotectedmethodeisok() {
+        int value1 = 10;
+        int value2 = 10;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.multiply(value1, value2) == 100);
+    }
+
+    @Test
+    public void Multiplikationpostivezahlnegativezahlis() {
+        int value1 = 50;
+        int value2 = -2;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.multiply(value1, value2) == -25);
+    }
+
+    @Test
+    public void Multiplikationvonnegativezahlisok() {
+        int value1 = -12;
+        int value2 = -12;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.multiply(value1, value2) == 144);
+    }
+
+    @Test
+    public void Multiplikationpositivezahlnndnullisok() {
+        int value1 = 7;
+        int value2 = 0;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.multiply(value1, value2) == 0);
+    }
+
+    @Test
+    public void Multiplikationnegativezahlnndnullisok() {
+        int value1 = -5;
+        int value2 = 0;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.multiply(value1, value2) == 0);
+    }
+
+    @Test
+    public void Multiplikationgrosstepositivezahlundkleinstenegativezahlistok() {
+        int value1 = Integer.MAX_VALUE;
+        int value2 = -1;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.multiply(value1, value2) == -Integer.MAX_VALUE);
+    }
+
+    @Test
+    public void Multiplikationgrösstepositiveundkleinstepositiveistok() {
+        int value1 = Integer.MAX_VALUE;
+        int value2 = 1;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.multiply(value1, value2) == 2147483647);
+    }
+
+    @Test
+    public void Multiplikationgrösstenegativekleinstepositiveistok() {
+        int value1 = Integer.MIN_VALUE;
+        int value2 = 1;
+        SaveCalculator testee = new SaveCalculator();
+        assertTrue(testee.multiply(value1, value2) == -2147483648);
     }
      
 
